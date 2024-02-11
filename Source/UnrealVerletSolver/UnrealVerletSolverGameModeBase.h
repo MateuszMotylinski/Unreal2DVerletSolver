e6371633111f380a20d25b711c5acc89ad884b88
MateuszMotylinski <development@mateuszmotylinski.com>
2024-02-11 18:26:51 +0000

    - Added initial files

---
+++ Source/UnrealVerletSolver/UnrealVerletSolverGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealVerletSolverGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALVERLETSOLVER_API AUnrealVerletSolverGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
