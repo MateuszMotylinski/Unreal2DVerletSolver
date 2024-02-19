bc4f0e8170f7415526dbdfd96bd838c3c3706167
MateuszMotylinski <development@mateuszmotylinski.com>
2024-02-19 23:59:24 +0000

    - Fixed simulation calculations

--- Source/UnrealVerletSolver/Private/Collision/CollisionSolver_Naive.cpp
+++ Source/UnrealVerletSolver/Private/Collision/CollisionSolver_Naive.cpp
@@ -35,44 +35,41 @@ bool UCollisionSolver_Naive::IsColliding(const FVector2D& vP1, const FVector2D& 
 
 void UCollisionSolver_Naive::HandleCollision(int32 iParticleIndex1, int32 iParticleIndex2)
 {
-
 	FVector2D& vP1 = PR_ParticlesData->arrPositions[iParticleIndex1];
 	FVector2D& vP2 = PR_ParticlesData->arrPositions[iParticleIndex2];
 
 	FVector2D& vVel1 = PR_ParticlesData->arrVelocities[iParticleIndex1];
 	FVector2D& vVel2 = PR_ParticlesData->arrVelocities[iParticleIndex2];
 
-	FVector2D DeltaPosition = vP2 - vP1;
+	float fP1Mass = PR_ParticlesData->fMass;
+	float fP2Mass = PR_ParticlesData->fMass;
+
+	// Find the distance between the two points of collision
+	double d = FMath::Sqrt(FMath::Pow(vP1.X - vP2.X, 2) + FMath::Pow(vP1.Y - vP2.Y, 2));
+
+	// Find the norm of the vector from the point of collision for the first circleand the point of collision of the second circle
+	FVector2D vNormal = (vP2 - vP1) / d;
+
+	// Calculate the p - value that takes into account the velocities of both circles
+	double p = 2 * (vVel1.X * vNormal.X + vVel1.Y * vNormal.Y - vVel2.X * vNormal.X - vVel2.Y * vNormal.Y) /
+		(fP1Mass + fP2Mass);
+
+	// Calculate the final velocity of each circle using each p - value.Note that each resultant is just the opposite sign with each variable replaced with the corresponding variable
+
+	vVel1 = vVel1 - p * fP1Mass * vNormal;
+	vVel2 = vVel2 + p * fP2Mass * vNormal;
 
 
-	float Distance = DeltaPosition.Size();
-	float RadiusSum = PR_ParticlesData->m_fParticlesRadius + PR_ParticlesData->m_fParticlesRadius;
+	// If after we aplied the force the circles are still colliding, then separate the
+	double fDist = FVector2D::Distance(vP1, vP2);
+	double fOverlap = PR_ParticlesData->m_fParticlesRadius + PR_ParticlesData->m_fParticlesRadius - fDist;
 
-	if (Distance < RadiusSum)
+	if (fOverlap > 0)
 	{
-		DeltaPosition.Normalize();
-		FVector2D Normal = DeltaPosition;
+		
+		FVector2D vMoveVector = (0.5f * fOverlap) * vNormal;
 
-
-		// Calculate overlap
-		float Overlap = RadiusSum - Distance;
-		// Calculate the correction vector to separate particles
-		FVector2D Correction = (0.5f * Overlap) * Normal;
-
-		// Move particles to resolve overlap
-		//vP1 -= Correction;
-		//vP2 += Correction;
-
-		FVector2D DeltaVelocity = vVel2 - vVel1;
-		float RelativeVelocity = FVector2D::DotProduct(DeltaVelocity, Normal);
-
-			float fP1Mass = PR_ParticlesData->fMass;
-			float fP2Mass = PR_ParticlesData->fMass;
-
-			float CoefficientOfRestitution = 1.0f;//0.8f;
-				FVector2D Impulse = (2.0f * RelativeVelocity / (fP1Mass + fP2Mass)) * Normal;
-
-				vVel1 += Impulse / fP1Mass;
-				vVel2 += -Impulse / fP2Mass;
+		vP1 -= vMoveVector;
+		vP2 += vMoveVector;
 	}
 }
--- Source/UnrealVerletSolver/Private/SolverActor.cpp
+++ Source/UnrealVerletSolver/Private/SolverActor.cpp
@@ -45,7 +45,7 @@ void ASolverActor::BeginPlay()
 
 	for (int32 i = 0; i < PR_iParticlesToSpawn; i++)
 	{
-		AddParticle(FVector2D(FMath::RandRange(0.0f, PR_fSimBoundingBoxWidth), FMath::RandRange(0.0f, PR_fSimBoundingBoxHeight)), FVector2D(FMath::RandRange(10, 100), FMath::RandRange(10, 100)));
+		AddParticle(FVector2D(FMath::RandRange(0.0f, PR_fSimBoundingBoxWidth), FMath::RandRange(0.0f, PR_fSimBoundingBoxHeight)), FVector2D(FMath::RandRange(10, 100), FMath::RandRange(0, 10)));
 	}
 }
 
@@ -83,6 +83,7 @@ void ASolverActor::UpdateSolver(float fDeltaTime)
 {   
 	for (int32 i = 0; i < m_xParticles.arrPositions.Num(); i++)
 	{
+
 		FVector2D& vCurrentPos = m_xParticles.arrPositions[i];
 		FVector2D& vPreviousPos = m_xParticles.arrPositionsPrev[i];
 		FVector2D& vAcceleration = m_xParticles.arrAccelerations[i];
@@ -90,31 +91,29 @@ void ASolverActor::UpdateSolver(float fDeltaTime)
 
 		// Verlet integration formula
 		FVector2D vTempPosition = m_xParticles.arrPositions[i];
-		//vCurrentPos += vCurrentPos - vPreviousPos + vAcceleration * FMath::Square(fDeltaTime);
+
 		vCurrentPos += vVelocity * fDeltaTime + 0.5f * vAcceleration * FMath::Square(fDeltaTime);
+
 		vPreviousPos = vTempPosition;
 
 		vVelocity += vAcceleration * fDeltaTime;
-		// Update velocity based on the change in position
-		//vVelocity = (vCurrentPos - vTempPosition) / fDeltaTime;
-
 	
 		// Check and correct for boundaries
 		if (vCurrentPos.X - m_fParticlesRadius < 0.0f || vCurrentPos.X + m_fParticlesRadius > PR_fSimBoundingBoxWidth)
 		{
-			vCurrentPos.X = FMath::Clamp(vCurrentPos.X, -m_fParticlesRadius, PR_fSimBoundingBoxWidth - m_fParticlesRadius);
-			vVelocity.X *= -PR_fRestitution;//vCurrentPos.X - (vCurrentPos.X - vPreviousPos.X);
+			vCurrentPos.X = FMath::Clamp(vCurrentPos.X, m_fParticlesRadius, PR_fSimBoundingBoxWidth - m_fParticlesRadius);
+			vVelocity.X = -vVelocity.X / 2;//PR_fRestitution;//vCurrentPos.X - (vCurrentPos.X - vPreviousPos.X);
 		}
 
 		if (vCurrentPos.Y - m_fParticlesRadius < 0.0f || vCurrentPos.Y + m_fParticlesRadius > PR_fSimBoundingBoxHeight)
 		{
-			vCurrentPos.Y = FMath::Clamp(vCurrentPos.Y, -m_fParticlesRadius, PR_fSimBoundingBoxHeight - m_fParticlesRadius);
-			vVelocity.Y *= -PR_fRestitution;//vCurrentPos.Y - (vCurrentPos.Y - vPreviousPos.Y);
+			vCurrentPos.Y = FMath::Clamp(vCurrentPos.Y, m_fParticlesRadius, PR_fSimBoundingBoxHeight - m_fParticlesRadius);
+			vVelocity.Y = -vVelocity.Y / 2; //PR_fRestitution;//vCurrentPos.Y - (vCurrentPos.Y - vPreviousPos.Y);
 		}
 
 
+		
 		PR_CollisionSolver->UpdateParticleCollision(i);
-
 
 	
 	}
@@ -133,7 +132,7 @@ void ASolverActor::OnConstruction(const FTransform& xTransform)
 void ASolverActor::AddParticle(const FVector2D& vStartPosition, const FVector2D& vStartVelocity)
 {
 	m_xParticles.arrPositions.Add(vStartPosition);
-	m_xParticles.arrAccelerations.Add(vStartVelocity);
+	m_xParticles.arrAccelerations.Add(FVector2D::One());
 	m_xParticles.arrPositionsPrev.Add(vStartPosition);
 	m_xParticles.arrVelocities.Add(vStartVelocity);
 }
