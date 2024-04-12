d3722c51d4dff3b7f9bb1da7c6a738c7d3cafb18
MateuszMotylinski <development@mateuszmotylinski.com>
2024-04-12 19:52:45 +0100

    - Set c++ standard to CPP20

--- Source/UnrealVerletSolver/UnrealVerletSolver.Build.cs
+++ Source/UnrealVerletSolver/UnrealVerletSolver.Build.cs
@@ -6,15 +6,18 @@ public class UnrealVerletSolver : ModuleRules
 {
 	public UnrealVerletSolver(ReadOnlyTargetRules Target) : base(Target)
 	{
+		
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "GeometryCore", "Niagara" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
+		CppStandard = CppStandardVersion.Cpp20;
+
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
-		
+
 		// Uncomment if you are using online features
 		// PrivateDependencyModuleNames.Add("OnlineSubsystem");
 
